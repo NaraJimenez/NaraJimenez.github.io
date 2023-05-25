70b3f2b0ad0c32289025fbb0e5acf0350ee37875
Nara Jimenez <narajimenez@gmail.com>
2023-05-25 15:30:17 +0200

    Retocada actividad M06 -5.1

--- js/js1.js
+++ js/js1.js
@@ -1,19 +1,14 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
-    window.addEventListener("click", (e) => {
-     
-      if (e.path[0].id === "gota") {
-        //Si se clica la imagen de la gota
-        //METER DIALOG
-        alert("¡Has clicado la gota de agua!");
-
-      } else if (e.path[0].id === "button") {
-        //Si se clica el boton
-        //METER DIALOG
-        alert("¡Has clicado el boton azul!");
-        
-      }
-    });
+  window.addEventListener("click", (e) => {
+    if (e.target.id === "gota") {
+      // Si se hace clic en la imagen de la gota
+      alert("¡Has clicado la gota de agua!");
+    } else if (e.target.id === "button") {
+      // Si se hace clic en el botón
+      alert("¡Has clicado el botón azul!");
+    }
+  });
   });
   
   
