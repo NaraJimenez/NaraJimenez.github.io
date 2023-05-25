2793d1f5e1c0bf4a4e7eca08e0b267afdb20611b
Nara Jimenez <narajimenez@gmail.com>
2023-05-25 17:12:52 +0200

    Retocado 05 y 05.1.2

--- js/js2.js
+++ js/js2.js
@@ -1,86 +1,81 @@
-(function() {
-
+document.addEventListener("DOMContentLoaded", function() {
     var todoApp = (function() {
-        document.addEventListener("DOMContentLoaded", function() {
-            init();
-        });
-
-        var init = function() {
-            document.getElementById("add-task").addEventListener("click", function() {
-                var newTitle = document.getElementById("title").value,
-                    newContent = document.getElementById("content").value;
-                if (validate(newTitle)) {
-                    addTodo(newTitle, newContent);
-                }
-            }, false);
-
-            document.getElementById("todos").addEventListener("click", function(e) {
-                if (e.target && e.target.nodeName == "BUTTON") {
-                    deleteTodo(e.target.parentNode);
-                }
-            }, false);
-        };
-
-        //Validación del campo de Añadir nueva tarea
-        var validate = function(newTitle) {
-            if (newTitle.length === 0) {
-                alert("Es necesario rellenar el campo");
-                return false;
-            } else {
-                return true;
-            }
-        };
-
-        //Se añade la tarea y se muestra
-        var addTodo = function(title, content) {
-            var newTodo = document.createElement("section"),
-                h2 = document.createElement("h2"),
-                newTitle = document.createTextNode(title),
-                p = document.createElement("p"),
-                newContent = document.createTextNode(content),
-                button = document.createElement("button"),
-                deleteBtn = document.createTextNode("Eliminar");
-
-            h2.appendChild(newTitle);
-            newTodo.appendChild(h2);
-            p.appendChild(newContent);
-            newTodo.appendChild(p);
-            button.appendChild(deleteBtn);
-            newTodo.appendChild(button);
-
-            document.getElementById("todos").appendChild(newTodo);
-            document.getElementById("form").reset();
-
-        };
-
-        //Eliminar la tarea
-        var deleteTodo = function(todo) {
-            todo.parentNode.removeChild(todo);
-        };
-    }());
-}());
-
-        var countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
-
-        // Cuenta atras por cada segundo
-        var x = setInterval(function() {
-
-        //Fehca actual
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
-
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-            
-        // Lo que mostramos por pantalla
-        document.getElementById("cuentaAtras").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
-            
-        // Mensaje si se acaba el tiempo
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("cuentaAtras").innerHTML = "'Nuevo Año, nuevos Propositos!";
+      init();
+  
+      function init() {
+        document.getElementById("add-task").addEventListener("click", function() {
+          var newTitle = document.getElementById("title").value;
+          var newContent = document.getElementById("content").value;
+          if (validate(newTitle)) {
+            addTodo(newTitle, newContent);
+          }
+        }, false);
+  
+        document.getElementById("todos").addEventListener("click", function(e) {
+          if (e.target && e.target.nodeName == "BUTTON") {
+            deleteTodo(e.target.parentNode);
+          }
+        }, false);
+      }
+  
+      function validate(newTitle) {
+        if (newTitle.length === 0) {
+          alert("Es necesario rellenar el campo");
+          return false;
+        } else {
+          return true;
         }
-        }, 1000);
+      }
+  
+      function addTodo(title, content) {
+        var newTodo = document.createElement("section");
+        var h2 = document.createElement("h2");
+        var newTitle = document.createTextNode(title);
+        var p = document.createElement("p");
+        var newContent = document.createTextNode(content);
+        var button = document.createElement("button");
+        var deleteBtn = document.createTextNode("Eliminar");
+  
+        h2.appendChild(newTitle);
+        newTodo.appendChild(h2);
+        p.appendChild(newContent);
+        newTodo.appendChild(p);
+        button.appendChild(deleteBtn);
+        newTodo.appendChild(button);
+  
+        document.getElementById("todos").appendChild(newTodo);
+        document.getElementById("form").reset();
+      }
+  
+      function deleteTodo(todo) {
+        todo.parentNode.removeChild(todo);
+      }
+  
+      return {
+        init: init,
+        addTodo: addTodo,
+        deleteTodo: deleteTodo
+      };
+    })();
+    
+    //Cuenta atras
+    var countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
+  
+    var x = setInterval(function() {
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+  
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+      document.getElementById("cuentaAtras").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("cuentaAtras").innerHTML = "¡Nuevo Año, nuevos Propósitos!";
+      }
+    }, 1000);
+  });
+  
