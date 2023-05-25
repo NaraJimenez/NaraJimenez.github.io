622558e89febe81a7dfd9a8e5c0fbe8af49ea161
Nara Jimenez <narajimenez@gmail.com>
2023-05-25 19:04:21 +0200

    Funciona actividad 05.2

--- js/js2.js
+++ js/js2.js
@@ -1,81 +1,72 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var todoApp = (function() {
-      init();
-  
-      function init() {
-        document.getElementById("add-task").addEventListener("click", function() {
-          var newTitle = document.getElementById("title").value;
-          var newContent = document.getElementById("content").value;
-          if (validate(newTitle)) {
-            addTodo(newTitle, newContent);
-          }
-        }, false);
-  
-        document.getElementById("todos").addEventListener("click", function(e) {
-          if (e.target && e.target.nodeName == "BUTTON") {
-            deleteTodo(e.target.parentNode);
-          }
-        }, false);
-      }
-  
-      function validate(newTitle) {
-        if (newTitle.length === 0) {
-          alert("Es necesario rellenar el campo");
-          return false;
-        } else {
-          return true;
-        }
-      }
-  
-      function addTodo(title, content) {
-        var newTodo = document.createElement("section");
-        var h2 = document.createElement("h2");
-        var newTitle = document.createTextNode(title);
-        var p = document.createElement("p");
-        var newContent = document.createTextNode(content);
-        var button = document.createElement("button");
-        var deleteBtn = document.createTextNode("Eliminar");
-  
-        h2.appendChild(newTitle);
-        newTodo.appendChild(h2);
-        p.appendChild(newContent);
-        newTodo.appendChild(p);
-        button.appendChild(deleteBtn);
-        newTodo.appendChild(button);
-  
-        document.getElementById("todos").appendChild(newTodo);
-        document.getElementById("form").reset();
-      }
-  
-      function deleteTodo(todo) {
-        todo.parentNode.removeChild(todo);
-      }
-  
-      return {
-        init: init,
-        addTodo: addTodo,
-        deleteTodo: deleteTodo
-      };
-    })();
+    
     
     //Cuenta atras
     var countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
-  
+
     var x = setInterval(function() {
-      var now = new Date().getTime();
-      var distance = countDownDate - now;
-  
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-      document.getElementById("cuentaAtras").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-  
-      if (distance < 0) {
+
+    document.getElementById("cuentaAtras").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
         clearInterval(x);
         document.getElementById("cuentaAtras").innerHTML = "¡Nuevo Año, nuevos Propósitos!";
-      }
+    }
     }, 1000);
-  });
-  
+
+    //Los propostios de Año nuevo
+    // Obtener referencias a los elementos del DOM
+    const addButton = document.getElementById('add-task');
+    const titleInput = document.getElementById('title');
+    const contentInput = document.getElementById('content');
+    const todosContainer = document.getElementById('todos');
+
+    // Función para agregar un nuevo propósito
+    function addTodo() {
+        // Obtener los valores de los campos de entrada
+        const title = titleInput.value;
+        const content = contentInput.value;
+
+        // Crear un nuevo elemento div para mostrar el propósito
+        const todoDiv = document.createElement('div');
+        todoDiv.className = 'todo-item';
+
+        // Crear elementos para mostrar el título, el contenido y el botón de borrar
+        const todoTitle = document.createElement('h3');
+        todoTitle.textContent = title;
+
+        const todoContent = document.createElement('p');
+        todoContent.textContent = content;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Borrar';
+        deleteButton.classList.add('btn');
+
+        // Evento de clic para borrar el propósito
+        deleteButton.addEventListener('click', function () {
+            todosContainer.removeChild(todoDiv);
+        });
+
+        // Agregar los elementos al div del propósito
+        todoDiv.appendChild(todoTitle);
+        todoDiv.appendChild(todoContent);
+        todoDiv.appendChild(deleteButton);
+
+        // Agregar el div del propósito al contenedor principal
+        todosContainer.appendChild(todoDiv);
+
+        // Limpiar los campos de entrada después de agregar el propósito
+        titleInput.value = '';
+        contentInput.value = '';
+    }
+
+    // Evento de clic para agregar un propósito cuando se hace clic en el botón "Añadir"
+    addButton.addEventListener('click', addTodo);
+});
